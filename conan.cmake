73435ca4b574612a46437b394fd13c6889a45312
Nikita Ivanov <niv@veeroute.com>
2018-02-08 13:32:29 +0300

    check if cxx flags empty

--- conan.cmake
+++ conan.cmake
@@ -151,10 +151,12 @@ endfunction()
 
 
 function(conan_cmake_detect_gnu_libcxx result)
-    string(FIND ${CMAKE_CXX_FLAGS} "-stdlib=libc++" stdlib_flag_position)
-    if (NOT stdlib_flag_position EQUAL -1)
-        set(${result} libc++ PARENT_SCOPE)
-        return()
+    if (${CMAKE_CXX_FLAGS})
+        string(FIND ${CMAKE_CXX_FLAGS} "-stdlib=libc++" stdlib_flag_position)
+        if (NOT stdlib_flag_position EQUAL -1)
+            set(${result} libc++ PARENT_SCOPE)
+            return()
+        endif()
     endif()
     
     # Allow -D_GLIBCXX_USE_CXX11_ABI=ON/OFF as argument to cmake
